3808dc5d5560c160a1ad75cc4db661206ff07a47
Fabien Loup <info@norydev.com>
2015-07-10 12:31:54 +0300

    model updated

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,11 @@ class Post
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  embeds_many :comments
+
   field :title, type: String
   field :body, type: String
-  field :starred, type: Mongoid::Boolean
-
+  field :starred, type: Boolean
 
   index({ starred: 1 })
 end
